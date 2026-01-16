319ebe7ec2cf3dcef7822141b09442af482d6012
Forward <Julia.Forward@k202domain.com>
2026-01-16 15:49:36 -0330

    Added comments & reviewed logic for all requirements

--- Labs/Lab 1/Lab1_Week2/Lab1_Week2_empty.py
+++ Labs/Lab 1/Lab1_Week2/Lab1_Week2_empty.py
@@ -1,3 +1,4 @@
+# Import required libraries
 import pandas as pd 
 import spacy
 from spacy.matcher import Matcher
@@ -5,14 +6,15 @@ import requests
 
 #INS. COMM. Observe the default natural language processing (NLP) pipeline
 nlp = spacy.blank("en")
-print("Pipeline components:", nlp.pipe_names) #Tokenizer is always available (even if the output shows blank)
+print("Pipeline components:", nlp.pipe_names) #INS. COMM. Tokenizer is always available (even if the output shows blank)
 
 #INS. COMM. HINT: Use the built-in add_pipe(...) command, which is a method of the Language object created by spacy.blank("en")
 #INS. COMM. HINT: You can use the built-in pipe_names command (a method of the Language object created by spacy.blank("en")) to view the current NLP pipeline
 #INS. COMM. GOAL: Add in the following components of the NLP pipeline
 
-#NOTE: Source had to be included for add_pipe, otherwise code throws initialization errors
+#NOTE: Source had to be included for add_pipe, otherwise code throws initialization errors for added pipeline components
 source_nlp = spacy.load("en_core_web_sm")
+
 #INS. COMM.        - PoS-tagging
 nlp.add_pipe("tagger", source=source_nlp)
 #INS. COMM.        - Parser
@@ -33,7 +35,8 @@ print(tokens)
 print()
 
 #INS. COMM.        - PoS-tagging
-# token.pos__ prints blank because spacy.blank("en") is a blank model
+# token.pos__ prints blank because spacy.blank("en") is a blank model 
+# with no POS library to reference for different speech part types
 pos_tokens = []
 for token in doc:
     pos_tokens.append(f"{token}: {token.pos_}")
@@ -44,6 +47,7 @@ print()
 #INS. COMM.        - Parsing
 # Parser fails to correctly parse the tokens due to the empty model 
 # lacking the vocabulary normally provided to the parser by a pre-trained model such as en_core_web_sm
+# resulting in most tokens being incorrectly identified as conj
 parsed_tokens = []
 for token in doc:
     parsed_tokens.append(f"{token}: {token.tag_}, {token.dep_}")
@@ -59,6 +63,7 @@ print("NER Results: ")
 print(ner_tokens)
 print("\n\n")
 
+#============================ Pre-Trained Model ==================================
 #INS. COMM. The following works with a pre-trained NLP pipeline
 nlp = spacy.load("en_core_web_sm")
 doc = nlp(text)
@@ -81,7 +86,7 @@ print(tokens)
 print()
 
 # POS Tagger/tagger
-# Each token is matched correctly to its part of speech such as 'morning' as a noun or 'Whitmore' as a proper noun
+# Each token is matched correctly to its part of speech such as 'morning' -> noun or 'Whitmore' -> proper noun
 pos_tokens = []
 for token in doc:
     pos_tokens.append(f"{token}: {token.pos_}")
@@ -99,7 +104,7 @@ print(parsed_tokens)
 print()
 
 # Attribute Ruler/attribute_ruler
-# Forces the text "Hampshire" to be recognized by POS tagging as an adjective, rather than a propernoun
+# Forces the text "Hampshire" to be recognized by POS tagging as an adjective, rather than a proper noun
 ruler = nlp.get_pipe("attribute_ruler")
 ruler.add(
     patterns=[[{"TEXT": "Hampshire"}]],
@@ -117,7 +122,7 @@ print(att_tokens)
 print()
 
 # Lemmatizer/lemmatizer
-# Hampshire becomes recognized as an adjective
+# Words are correctly reduced to their base forms ex. "met" -> meet
 lemma = [token.lemma_ for token in doc]
 print("Lemmatization Results: ")
 print(lemma)
