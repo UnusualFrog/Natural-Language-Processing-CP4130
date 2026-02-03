9361937976e942b52a80aaa8f128b786599c7954
UnusualFrog <forwardnoah@gmail.com>
2026-02-03 13:43:09 -0330

    Starting Lab 2

---
+++ Labs/Lab 2/Lab4130_Week4_empty.py
@@ -0,0 +1,36 @@
+def load_noBoW():
+    #load the dataset without considering the Bag-of-Words model
+
+    return
+
+def load_BoW():
+    #load the dataset while considering the Bag-of-Words model
+    
+    return
+
+def SVM():
+    #Implement Support Vector Machine (SVM) without considering Bag-of-Words model
+    #Feel free to use built-in functions
+
+    #Implement Support Vector Machine (SVM) while considering Bag-of-Words model
+    #Feel free to use built-in functions
+    
+    return
+
+def LR():
+    #Implement Logistic Regression (LR) without considering Bag-of-Words model
+    #Feel free to use built-in functions
+
+    #Implement Logistic Regression (LR) while considering Bag-of-Words model
+    #Feel free to use built-in functions
+    
+    return
+
+def main():
+    load_noBoW()
+    BoW()
+    SVM()
+    LR()
+
+if __name__ == "__main__":
+    main()
