e1da1a51bd8d2170c54a205343a89327e25ab5e5
UnusualFrog <forwardnoah@gmail.com>
2026-02-04 16:56:52 -0330

    Prototype for final project lyric genre prediction; updated gitignore to ignore music dataset

---
+++ FinalProject/test_genius.py
@@ -0,0 +1,8 @@
+import lyricsgenius
+
+token = "1Y4W8rda4lIgTZL8djnafAnIrZqH-YsiyxVRmdx7Cii1m_h5XO-uWzwjxhnRZZVl"
+
+genius = lyricsgenius.Genius(token)
+
+artist = genius.search_artist("Gorillaz", max_songs=3, sort="title")
+print(artist.songs)
---
+++ FinalProject/test_load.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df_train = pd.read_csv("ds2.csv",  encoding="latin1")
+
+print(df_train.head(5))
