afc8a22d4c63ce8d53262a20e3307b16aa8670cb
UnusualFrog <forwardnoah@gmail.com>
2026-02-24 17:35:45 -0330

    Completed lab 4 week 7 code

--- Labs/Lab 5 (Lab 4 Week 7)/lab4.py
+++ Labs/Lab 5 (Lab 4 Week 7)/lab4.py
@@ -1,14 +1,13 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import confusion_matrix
 
 # Do not truncate pandas output columns
-pd.set_option('display.max_columns', None)  
+pd.set_option('display.max_columns', None)
 
 # Load data
 fake_data = pd.read_csv("Fake.csv",  encoding="latin1")
@@ -30,20 +29,31 @@ true_data = true_data.assign(target=true_label)
 data_frames = [fake_data, true_data]
 data = pd.concat(data_frames)
 
+# Seperate target from training features
 target = data["target"]
 data = data.drop("target", axis=1)
 
-print(target)
-print(data)
+# Combine title and text into single feature for vectorization
+data["title_text"] = data["title"] + data["text"]
+data = data.drop("title", axis=1)
+data = data.drop("text", axis=1)
+
+# Drop irrelevant features
+data = data.drop("date", axis=1)
+data = data.drop("subject", axis=1)
+
+# print(target)
+# print(data)
 
 # Train/Test Split with 30% test data
-X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(data["title_text"], target, test_size=0.3, random_state=42)
 
 # Create CountVectorizer Object
+n_gram = (1, 3)
 vectorizer = CountVectorizer(
     stop_words="english",
-    ngram_range=(1, 2),
-    max_features=20
+    ngram_range=n_gram,
+    max_features=5000
 )
 
 # Pass data through pre-processing pipeline
@@ -59,11 +69,13 @@ y_pred = model.predict(X_test_vec)
 
 cm = confusion_matrix(y_test, y_pred)
 
-sns.heatmap(cm, 
-            annot=True,
-            fmt='g', 
-            xticklabels=['True News','Fake News'],
-            yticklabels=['True News','Fake News'])
+# Generate confusion matrix plot
+sns.heatmap(
+            cm, annot=True, fmt='g',
+            xticklabels=['Fake News', 'True News'],
+            yticklabels=['Fake News', 'True News']
+            )
+
 plt.ylabel('Actual', fontsize=13)
 plt.title('Confusion Matrix', fontsize=17, pad=20)
 plt.gca().xaxis.set_label_position('top') 
@@ -71,7 +83,7 @@ plt.xlabel('Prediction', fontsize=13)
 plt.gca().xaxis.tick_top()
 
 plt.gca().figure.subplots_adjust(bottom=0.2)
-plt.gca().figure.text(0.5, 0.05, 'Prediction', ha='center', fontsize=13)
+plt.gca().figure.text(0.5, 0.05, 'Confusion Matrix with ngram_range: ' + str(n_gram), ha='center', fontsize=13)
 plt.show()
 
 
