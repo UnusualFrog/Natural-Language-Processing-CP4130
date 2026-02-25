5b91102f161bcdcd6085c8a1a9c8771b27a52ff9
Forward <Julia.Forward@k202domain.com>
2026-02-25 15:57:35 -0330

    A2 progress

--- Assignment/A2/a2.py
+++ Assignment/A2/a2.py
@@ -1,6 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import pandas as pd
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
 
 class Perceptron:
     def __init__(self, learning_rate=0.1, epochs=20):
@@ -43,3 +48,62 @@ class Perceptron:
                 errors += int(update != 0)
             # track total incorrect predictions per epoch
             self.errors_per_epoch.append(errors)
+            
+def main():
+    # Load data
+    data = pd.read_csv("spam.csv",  encoding="latin1")
+    
+    data["target"] = data["v1"].apply(lambda x: 1 if x == "spam" else 0)
+
+    # print(data.head())
+
+    # Split data into train feature and target label
+    target = data["target"]
+    train = data["v2"]
+
+    # print(target)
+    # print(train)
+
+    # Split data into train/test sets
+    X_train, X_test, y_train, y_test = train_test_split(train, target, test_size=0.2, random_state=42)
+    
+    # Create CountVectorizer Object
+    n_gram = (2, 2)
+    vectorizer = CountVectorizer(
+        stop_words="english",
+        ngram_range=n_gram,
+        max_features=5000
+    )
+    
+    # Pass data through pre-processing pipeline and convert from sparse matrix to dense array for downstream processing
+    X_train_vec = vectorizer.fit_transform(X_train).toarray()
+    X_test_vec = vectorizer.transform(X_test).toarray()
+    
+    model = Perceptron()
+    model.fit(X_train_vec, y_train)
+    
+    y_pred = model.predict(X_test_vec)
+    
+    print(classification_report(y_test, y_pred))
+    
+    cm = confusion_matrix(y_test, y_pred)
+
+    # Generate confusion matrix plot
+    sns.heatmap(
+                cm, annot=True, fmt='g',
+                xticklabels=['Ham', 'Spam'],
+                yticklabels=['Ham', 'Spam']
+                )
+
+    plt.ylabel('Actual', fontsize=13)
+    plt.title('Confusion Matrix', fontsize=17, pad=20)
+    plt.gca().xaxis.set_label_position('top') 
+    plt.xlabel('Prediction', fontsize=13)
+    plt.gca().xaxis.tick_top()
+
+    plt.gca().figure.subplots_adjust(bottom=0.2)
+    plt.gca().figure.text(0.5, 0.05, 'Confusion Matrix with ngram_range: ' + str(n_gram), ha='center', fontsize=13)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
